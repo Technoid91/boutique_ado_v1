1800a70b593b4f9269f11660a275f5b8dd47b4cd
Technoid <Technoid@Sazonov.station>
2024-03-26 14:13:26 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+
+        'stripe_public_key': 'pk_test_51OyZyRGlomcvb3PUq4dG9M0Y1v7E3y9okwmkp8x7UwirDYVMHq9Shc9FP5QeVRH57FYexuk1X1NumqKtPjxqMH7C005YBEJR6Q',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
